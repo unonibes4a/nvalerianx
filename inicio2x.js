ecc223cb34ae8210071eddd62cfe74781b1b5425
unonibes4a <51521414+unonibes4a@users.noreply.github.com>
2020-03-21 00:11:31 -0500

    Add files via upload

--- inicio2x.js
+++ inicio2x.js
@@ -2557,15 +2557,7 @@ if(sturl=="https://pelisplus.me/series/"){
   if(sturl=="https://repelisgo.net/"){
     var estesxripseva='setTimeout(function(){ try { document.getElementById("buttons").style.display="none"; }catch (error) { } },500); try { document.getElementById("buttons").style.display="none"; }catch (error) { } try { document.getElementsByClassName("resizer")[0].style.display="none"; }catch (error) { } setTimeout(function(){ try { document.getElementsByTagName("img")[0].src="https://github.com/unonubes19/imgsolo/blob/master/varpng.png?raw=true"; }catch (error) { } },500);';
     sevaconscrip(sturl,estesxripseva);}   
-  
-    if(sturl=="https://www.repeliss.tv/"){
 
-
-    var estesxripseva='';
-    
-    sevaconscrip(sturl,estesxripseva);
-  }   
-  
   
   
 
